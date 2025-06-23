68f84bf846f414e31d3011f4c29f1d8fa5a43183
Debanka Khan <debankakhan926@gmail.com>
2025-06-23 09:33:29 +0530

    Story 3.2 Menu EndPoints Created

---
+++ src/main/java/com/ReactiveEats/FoodOrderApplication/service/MenuService.java
@@ -0,0 +1,26 @@
+package com.ReactiveEats.FoodOrderApplication.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ReactiveEats.FoodOrderApplication.entities.MenuItem;
+import com.ReactiveEats.FoodOrderApplication.repository.MenuRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MenuService {
+	
+	@Autowired
+	private MenuRepository menuRepository;
+	
+	public Mono<String> addMenu(MenuItem menuItem){
+		return menuRepository.save(menuItem).map(x -> "Menu saved for: "+x.getName());
+	}
+	
+	public Flux<MenuItem> getMenusByCuisine(String cuisine){
+		return menuRepository.findByCuisine(cuisine);
+	}
+
+}
